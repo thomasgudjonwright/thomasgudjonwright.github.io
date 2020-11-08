f5e18ca76e9af28c07fb583b5e33b33cd13a4a01
Tom Wright <tom.wright@invenia.ca>
2020-11-08 00:12:33 -0600

    First change: add welcome and delete redundent files

---
+++ index.js
@@ -0,0 +1,17 @@
+// var i = 0;
+// var txt = "Hi, I'm Tom. Welcome to my site.";
+// var speed = 80;
+// var speed2 = 700;
+
+// function welcomeGuest() {
+//   if (i < txt.length) {
+//     document.getElementById("welcome").innerHTML += txt.charAt(i);
+//     i++;
+//     if (i==12) {
+//       setTimeout(welcomeGuest, speed2);
+//     }
+//     else {
+//       setTimeout(welcomeGuest, speed);
+//     }
+//   }
+// }
